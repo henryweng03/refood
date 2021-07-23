4bcaf3d5ac95105f58b8ead017e45bb78bf1025b
PitapatCat <kevwjin@gmail.com>
2021-07-23 13:57:18 -0700

    Finish bare UI of landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { TopNav } from './TopNav/TopNav';
+import styles from './LandingPage.module.css';
+import Typography from '@material-ui/core/Typography';
 
 export function LandingPage() {
 	return (
-		<div>
-			<TopNav/>
-		</div>
+    <div className={styles['content-container']}>
+      <TopNav />
+      <Typography
+        style={{ fontWeight: 600 }}
+        align='center'
+        variant='h2'
+      >
+        ReFood
+      </Typography>
+    </div>
 	);
 }
