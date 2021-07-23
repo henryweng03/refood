fd08500b10de2fc7d4b39ef911abf79f6d486c49
Henry Weng <henryweng2003@gmail.com>
2021-07-23 13:12:24 -0700

    Added form to add map markers

--- src/Store/StoreApp.js
+++ src/Store/StoreApp.js
@@ -1,4 +1,6 @@
 import React from "react";
+import './store.css';
+import { useForm } from "react-hook-form";
 import {
   GoogleMap,
   useLoadScript,
@@ -19,22 +21,53 @@ const options = {
   disableDefaultUI: true,
   zoomControl: true,
 }
+
 export default function StoreApp() {
   const {isLoaded, loadError} = useLoadScript({
    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
    libraries,
   });
 
+  const [markers, setMarkers] = React.useState([]);
+
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        data,
+        time: new Date(),
+      }
+    ])
+    document.getElementById("add-marker-form").reset();
+  }
+
   if (loadError) return "Error loading maps";
   if(!isLoaded) return "Loading maps";
 
   return <div>
-    <h1>Refood </h1>
+    <h1>Refood🥬</h1>
     <GoogleMap 
       mapContainerStyle = {mapContainerStyle}
       zoom = {10}
       center = {center}
-      options = {options}>
+      options = {options}
+    >
+      {/* renders markers on the map */}
+      {markers.map((marker) => (
+        <Marker
+          key = {marker.time.toISOString()}
+          position = {{lat: parseFloat(marker.data.lat), lng: parseFloat(marker.data.lng)}}
+          />
+      ))}
     </GoogleMap>
+    <p></p>
+    <form onSubmit = {handleSubmit(onSubmit)} id = "add-marker-form">
+      <label htmlFor = "lat">Latitude:</label>
+      <input type = "number" step = "any" {...register("lat")} /><br/>
+      <label htmlFor = "lng">Longitude:</label>
+      <input type = "number" step = "any" {...register("lng")} /><p/>
+      <input type = "submit" />
+    </form>
   </div>
 }
