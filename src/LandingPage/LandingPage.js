0946163598978bf7abd9fb0c8b8c38d0ba95ec6e
Henry Weng <henryweng2003@gmail.com>
2021-07-24 01:55:57 -0700

    Restyled landing page, added routing

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,41 +1,18 @@
 import React from "react";
+import "../index.css"
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 export default function StoreApp() {
-    const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm();
 
-    return <div>
-      <h1>Landing Page</h1><br />
-
-      <div class = "create-event">
-        <h2 class = "form-header">Create New Event</h2>
-        <form id = "add-marker-form">
-          <div class = "field">
-           <input type = "number" step = "any" required {...register("lat")} /><br/><br/>
-           <label>Latitude</label>
-          </div>
-          <div class = "field">
-           <input type = "number" step = "any" required {...register("lng")} /><br/><br/>
-           <label>Longitude</label>
-          </div>
-          <div class = "field">
-           <input type = "text" required {...register("name")} /><br/><br/>
-           <label>Event name</label>
-          </div>
-          <div class = "field">
-           <input type = "datetime-local" required {...register("startDate")} /><br/><br/>
-           <label>Event start</label>
-          </div>
-          <div class = "field">
-           <input type = "datetime-local" required {...register("endDate")} /><br/><br/>
-           <label>Event end</label>
-          </div>
-          <div class = "field">
-            <input type = "text" required {...register("description")} /><br/>
-           <label>Event description</label>
-          </div>
-           <input type = "submit" />
-      </form>
-      </div>
+  return <div id = "container">
+      <img class = 'lp-logo' src="refood_transparent.png" alt="Refood Logo" />
+      <Link to = "/">
+        <button className = 'landing-button'>Sign in as Store</button>
+      </Link>
+      <Link to = "/">
+        <button className = 'landing-button'>Sign in as Individual</button>
+      </Link>
     </div>
   }
